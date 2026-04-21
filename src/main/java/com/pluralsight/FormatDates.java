4c29f296c656066caa532991ab770656d9c37121
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-21 15:17:52 -0400

    Added all date formatting requirements commit

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class FormatDates {
     public static void main(String[] args) {
@@ -11,5 +12,21 @@ public class FormatDates {
         String formatted1 = format1.format(currentDate);
         System.out.println(formatted1);
 
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
+        String formatted2 = format2.format(currentDate);
+        System.out.println(formatted2);
+
+        SimpleDateFormat format3 = new SimpleDateFormat("EEEE dd, yyyy");
+        String formatted3 = format3.format(currentDate);
+        System.out.println(formatted3);
+
+        SimpleDateFormat format4 = new SimpleDateFormat("EEEE, MMM dd, yyyy HH:mm");
+        format4.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String formatted4 = format4.format(currentDate);
+        System.out.println(formatted4);
+
+        SimpleDateFormat format5 = new SimpleDateFormat("h:mm on dd-MMM-yyyy");
+        String formatted5 = format5.format(currentDate);
+        System.out.println(formatted5);
     }
 }
